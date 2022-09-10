328d7c186216ab3eae88e27abf4e4717a2442992
Rishabh Katkar <89900387+Rishabhk3110@users.noreply.github.com>
2022-09-10 21:34:43 +0530

    Update Note.js

--- src/Components/Parts/Note.js
+++ src/Components/Parts/Note.js
@@ -5,11 +5,11 @@ function Note({ id, text, deleteNote }) {
     <div className="note">
       <div className="note__body">{text}</div>
       <div className="note__footer" style={{ justifyContent: "flex-end" }}>
-      <MdDelete>
+      <MdDelete
         className="note__delete"
           onClick={() => deleteNote(id)}
           aria-hidden="true"
-          </MdDelete></div>
+          ></MdDelete></div>
       </div>
 
   );
